81aba97f04624a0cecdbc729b28ecd9137c46868
kokeeh794 <pokemonjorge007@gmail.com>
2025-05-05 09:04:03 +0200

    Version 1
    Version 1

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$db = 'algegym';
+$user = 'root';
+$pass = '';
+
+try {
+  $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  die("Error de conexión: " . $e->getMessage());
+}
+?>
