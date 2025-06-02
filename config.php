e528a8b90d2f19927ed7636fe9c5b404336b0d66
kokeeh794 <pokemonjorge007@gmail.com>
2025-06-02 09:00:44 +0200

    Version Final
    Version Final

--- config.php
+++ config.php
@@ -8,6 +8,7 @@ try {
   $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-  die("Error de conexión: " . $e->getMessage());
+  die("Error de conexion: " . $e->getMessage());
 }
 ?>
+
